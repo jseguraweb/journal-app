8b4a1d4f40833f3862a21955a7f69c315aa17781
Jaime Segura <jseguraweb@gmail.com>
2020-03-02 17:20:30 +0100

    improve boxes and navigation menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 import '../style/Navigation.scss'
+import { FaBars } from 'react-icons/fa'
 
 export default class Navigation extends Component {
+    state = {
+        menuOpen: false
+    }
+
+    toggleMenu = e => {
+        let navMenu = e.currentTarget.nextElementSibling;
+        if (this.state.menuOpen) {
+            this.setState({
+                menuOpen: false
+            })
+            navMenu.style.opacity = 0
+            navMenu.style.zIndex = -1
+        } else {
+            this.setState({
+                menuOpen: true
+            })
+            navMenu.style.opacity = 1
+            navMenu.style.zIndex = 2
+        }
+    }
+
     render() {
+
         return (
             <nav>
                 <h1>Journal App</h1>
+                <FaBars className="icon-menu" onClick={this.toggleMenu} />
                 <ul>
                     <li><a href="#">Politics</a></li>
                     <li><a href="#">Economics</a></li>
