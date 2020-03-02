53cd2e47e14aa325a90c003e401227e8b5b76953
Jaime Segura <jseguraweb@gmail.com>
2020-03-02 16:44:01 +0100

    create boxes for news

--- src/components/ContainerOfNews.js
+++ src/components/ContainerOfNews.js
@@ -3,17 +3,48 @@ import '../style/ContainerOfNews.scss'
 import NewBox from './NewBox'
 
 export default class ContainerOfNews extends Component {
+    state = ({
+        news: []
+    })
+
+    componentDidMount = async () => {
+        try {
+            const serverData = await fetch('http://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=701dc18d676e4d62a0c678f128ece113');
+            const response = await serverData.json();
+            this.setState({
+                news: response.articles
+            })
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     render() {
+
+        const { news } = this.state
+        let allArticles;
+        if (news.length > 0) {
+            allArticles = news.map((article, i) => {
+                if (!article.description.includes('http')) {
+                    return (
+                        <div className="box" key={i}><NewBox key={i} article={article} /></div>
+                    )
+                }
+            })
+        }
+
+
         return (
-            <div className="container">
-                <div className="box box1"><NewBox /></div>
+            <div className="container" >
+                {allArticles}
+                {/* <div className="box box1"><NewBox /></div>
                 <div className="box box2"><NewBox /></div>
                 <div className="box box3"><NewBox /></div>
                 <div className="box box4"><NewBox /></div>
                 <div className="box box5"><NewBox /></div>
                 <div className="box box6"><NewBox /></div>
                 <div className="box box7"><NewBox /></div>
-                <div className="box box8"><NewBox /></div>
+                <div className="box box8"><NewBox /></div> */}
             </div>
         )
     }
--- src/components/NewBox.js
+++ src/components/NewBox.js
@@ -3,11 +3,22 @@ import '../style/NewBox.scss'
 
 export default class NewBox extends Component {
     render() {
+        console.log(this.props);
+        let article = this.props.article;
+        let title = article.title;
+        let description = article.description;
+        let image = article.urlToImage;
+        let url = article.url;
+
         return (
             <article>
-                <h3>Title of the new</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, at. Tempore at odio eligendi, veniam nobis distinctio doloribus eos modi!
+                <h3 className="title">{title}</h3>
+                <div className="image" style={{ backgroundImage: `url("${image}")` }}></div>
+                <p className="description">
+                    {description}
+                </p>
+                <p className="link">
+                    <a href={url}>Continue reading</a>
                 </p>
             </article>
         )
