a005a3aacab718f2b938659e3d457ee52a80af22
Jaime Segura <jseguraweb@gmail.com>
2020-03-02 22:47:45 +0100

    Updates

---
+++ precache-manifest.1c5599bbf405f599ffb634007c79705e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f7f335f3875138b55c5d1e4ee5e396",
+    "url": "/journal-app/index.html"
+  },
+  {
+    "revision": "6c9ab587620dcc793603",
+    "url": "/journal-app/static/css/main.66f9e99b.chunk.css"
+  },
+  {
+    "revision": "a8c2ba219de77b99f824",
+    "url": "/journal-app/static/js/2.c3ab018d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/journal-app/static/js/2.c3ab018d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9ab587620dcc793603",
+    "url": "/journal-app/static/js/main.f7f72b09.chunk.js"
+  },
+  {
+    "revision": "4c227560050a10c4d27d",
+    "url": "/journal-app/static/js/runtime-main.a764067a.js"
+  }
+]);
