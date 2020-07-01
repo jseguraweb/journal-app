6e7ac9038f9dc07dcb270991ea61cc493c4a46e6
Jaime Segura <jseguraweb@gmail.com>
2020-07-01 16:51:40 +0200

    add category world

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,15 +12,16 @@ const Navigation = ({ changeCategory, category }) => {
     return (
         <animated.div style={appearFromTop}>
             <header>
-                <h1 onClick={() => changeCategory('business')}>The Journal App</h1>
+                <h1 onClick={() => changeCategory('Business')}>The Journal App</h1>
                 {
                     menuOpen ? <FaTimes className="icon-menu" onClick={() => setMenuOpen(!menuOpen)} /> : <FaBars className="icon-menu" onClick={() => setMenuOpen(!menuOpen)} />
                 }
             </header>
             <animated.nav style={appearanceMenu}>
                 <ul>
+                    <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('World') }} style={{ backgroundColor: category === 'World' ? '#fff' : '#000', color: category === 'World' ? '#000' : '#fff' }}>WORLD</li>
                     <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('Business') }} style={{ backgroundColor: category === 'Business' ? '#fff' : '#000', color: category === 'Business' ? '#000' : '#fff' }}>BUSINESS</li>
-                    <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('Sport') }} style={{ backgroundColor: category === 'Sport' ? '#fff' : '#000', color: category === 'Sport' ? '#000' : '#fff' }}>SPORT</li>
+                    <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('Sports') }} style={{ backgroundColor: category === 'Sports' ? '#fff' : '#000', color: category === 'Sports' ? '#000' : '#fff' }}>SPORT</li>
                     <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('Entertainment') }} style={{ backgroundColor: category === 'Entertainment' ? '#fff' : '#000', color: category === 'Entertainment' ? '#000' : '#fff' }}>ENTERTAINMENT</li>
                     <li onClick={() => { setMenuOpen(!menuOpen); changeCategory('Technology') }} style={{ backgroundColor: category === 'Technology' ? '#fff' : '#000', color: category === 'Technology' ? '#000' : '#fff' }}>TECHNOLOGY</li>
                 </ul>
